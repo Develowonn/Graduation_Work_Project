e084f4d4c2844c1a70336ccebf41d89a499cf34d
sangw0n <pilwon070127@naver.com>
2025-06-16 23:40:41 +0900

    수정 | 여우 모델링 움직임 적용완료

--- Assets/00. Src/Scripts/CameraFollowTarget.cs
+++ Assets/00. Src/Scripts/CameraFollowTarget.cs
@@ -14,7 +14,7 @@ public class CameraFollowTarget : MonoBehaviour
         transform.position = target.position + offset;
     }
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
         Vector3 targetPosition = target.position + offset;
         transform.position     = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * followSpeed);
