0f998ca68f3fdfd3e006a6b03ec2eee002b5f002
sangw0n <pilwon070127@naver.com>
2025-06-22 20:17:07 +0900

    추가 | 토네이도 스킬 및 파일 구조 변경

---
+++ Assets/00. GameAsset/00. Src/Scripts/Manager/DeckManager.cs
@@ -0,0 +1,91 @@
+// # System
+using System.Collections.Generic;
+
+// # Unity
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    [SerializeField]
+    private int         maxDeckSize;
+    [SerializeField]
+    private Transform   deckParent;
+
+    [Header("UI")]
+    [SerializeField]
+    private Transform   myDeckUIParents;
+    private Animator    myDeckAnimator;
+    private bool        isUsingDeck;
+
+    private List<Card>  myDeck;
+    private Card        selectedCard;
+
+    private void Start()
+    {
+        myDeck = new List<Card>();
+
+        myDeckAnimator = myDeckUIParents.GetComponent<Animator>();
+
+        InitializeDeck();
+
+        myDeck[0].Execute();
+    }
+
+	private void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+			isUsingDeck = isUsingDeck != true;
+			myDeckAnimator.SetBool("IsUsingDeck", isUsingDeck);
+		}
+    }
+
+	private void InitializeDeck()
+    {
+        for(int i = 0; i < maxDeckSize; i++)
+        {
+            Card card = CardManager.Instance.GetRandomCard();
+            card.transform.SetParent(deckParent);
+
+			myDeck.Add(card);
+        }
+    }
+
+    public void UseCard()
+    {
+        selectedCard.Execute();
+    }
+
+    public void SelectCard(string cardName)
+    {
+        Card card = myDeck.Find(x => x.GetCardName() == cardName);
+
+        if (card != null) selectedCard = card;
+        else
+        {
+            Debug.LogWarning($"DeckManager_{cardName} 카드를 찾을 수 없습니다");
+        }
+    }
+
+    public void RemoveCard(string cardName)
+    {
+        Card card = myDeck.Find(x => x.GetCardName() == cardName);
+
+        if (card != null) myDeck.Remove(card);
+        else
+        {
+            Debug.LogWarning($"DeckManager_{cardName} 카드를 찾을 수 없습니다");
+        }
+    }
+
+    public void RemoveCard(Card card)
+    {
+        Card _card = myDeck.Find(x => x == card);
+
+        if (card != null) myDeck.Remove(_card);
+        else
+        {
+            Debug.LogWarning($"DeckManager_{card.GetCardName()} 카드를 찾을 수 없습니다");
+        }
+    }
+}
---
+++ Assets/00. GameAsset/00. Src/Scripts/MovementRigidbody.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MovementRigidbody : MonoBehaviour
+{
+    public Vector3      MovementDir { get; private set; }
+
+    [SerializeField]
+    private float       movementSpeed;
+    private Rigidbody   rigid;
+
+    private void Start()
+    {
+        rigid    = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.linearVelocity = MovementDir.normalized * movementSpeed;
+    }
+
+    public void MoveTo(Vector3 moveDir)
+    {
+        MovementDir = moveDir;
+    }
+
+	public void MoveToLocal(Vector3 moveDir)
+	{
+		MovementDir = transform.TransformDirection(moveDir);
+	}
+}
