39e8d36a3db0078d720c7662433d89c1626241cf
pilwon <131760662+Develowonn@users.noreply.github.com>
2025-06-24 18:16:46 +0900

    수정 | 토네이도 폭발 연출 안보이는 점

--- Assets/00. GameAsset/00. Src/Scripts/Card/AttackCard/TornadoCard/TornadoCard.cs
+++ Assets/00. GameAsset/00. Src/Scripts/Card/AttackCard/TornadoCard/TornadoCard.cs
@@ -22,8 +22,6 @@ public class TornadoCard : Card
 	private float			radius;
 	[SerializeField]
 	private float			explosionTime;
-	[SerializeField]
-	private float			explosionDestroyTime;
 
 	[Header("Offset")]
 	[SerializeField]
@@ -84,8 +82,6 @@ public class TornadoCard : Card
 	{
 		explosion = Instantiate(ExplosionVfx, InGameManager.Instance.GetPlayerObject().transform.position + auraOffset, Quaternion.Euler(-90, 0, 0));
 		explosion.transform.SetParent(InGameManager.Instance.GetPlayerObject().transform);
-
-		Destroy(explosion, explosionDestroyTime);
 	}
 
 	private void SpawnTornado()
