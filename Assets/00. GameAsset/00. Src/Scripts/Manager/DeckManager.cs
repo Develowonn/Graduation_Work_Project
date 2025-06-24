32c436726d6e3d2c9f453c6c1aa55252c467e3db
pilwon <131760662+Develowonn@users.noreply.github.com>
2025-06-24 12:00:23 +0900

    추가 | 카드 쿨타임

--- Assets/00. GameAsset/00. Src/Scripts/Manager/DeckManager.cs
+++ Assets/00. GameAsset/00. Src/Scripts/Manager/DeckManager.cs
@@ -107,6 +107,12 @@ public class DeckManager : MonoBehaviour
 		await UniTask.Delay(TimeSpan.FromSeconds(cardUiList[selectedCardIndex].GetUsedMoveDuration() + usedDisappearDelay));
 		cardUiList[selectedCardIndex].gameObject.SetActive(false);
 
+		// 카드 쿨타임 UI에 표시
+		foreach(var cardUI in cardUiList)
+        {
+			cardUI.ProcessCooltime(cardCooltime).Forget();
+        }
+
 		await UniTask.Delay(TimeSpan.FromSeconds(cardCooltime));
 		isUsingCard = false;
 	}
