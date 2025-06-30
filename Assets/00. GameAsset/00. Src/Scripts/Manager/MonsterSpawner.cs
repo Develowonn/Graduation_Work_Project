d7612b1702d2bd5b684936dee4bd748d34e9362d
sangw0n <pilwon070127@naver.com>
2025-06-30 21:49:42 +0900

    추가 | 설정한 스폰 데이터에 맞게 몬스터 스폰

--- Assets/00. GameAsset/00. Src/Scripts/Manager/MonsterSpawner.cs
+++ Assets/00. GameAsset/00. Src/Scripts/Manager/MonsterSpawner.cs
@@ -1,19 +1,31 @@
 // # System
+using System;
 using System.Collections.Generic;
 
 // # Unity
 using UnityEngine;
 
+// # Etc
+using Cysharp.Threading.Tasks;
+
 public class MonsterSpawner : MonoBehaviour
 {
 	[SerializeField]
 	private TextAsset			   monsterSpawnDataJson;
-
+	[SerializeField]
 	private List<MonsterSpawnData> monsterSpawnDatas;
 
+	private decimal				   elapsedTime;
+
+	private GameObject			   player;
+ 
 	private void Start()
 	{
+		player = InGameManager.Instance.GetPlayerObject();
+
 		InitializeMonsterSpawnData();
+
+		SpawnMonster().Forget();
 	}
 
 	private void InitializeMonsterSpawnData()
@@ -22,5 +34,24 @@ public class MonsterSpawner : MonoBehaviour
 
 	    monsterSpawnDatas						  = new List<MonsterSpawnData>(); 
 		monsterSpawnDatas						  = monsterSpawnDataList.monsterSpawnDatas;
+		monsterSpawnDatas.Sort((x, y) => x.spawnTime.CompareTo(y.spawnTime));
+	}
+
+	private async UniTaskVoid SpawnMonster()
+	{
+		while (true)
+		{
+			foreach(var monsterSpawnData in monsterSpawnDatas)
+			{
+				if(monsterSpawnData.spawnTime != elapsedTime)
+					break;
+
+				Vector3 spawnPos = player.transform.position + monsterSpawnData.spawnPosition;
+				ObjectPool.instance.SpawnFromPool(monsterSpawnData.monsterName, spawnPos);
+			}
+
+			elapsedTime += 1.0m;
+			await UniTask.Delay(TimeSpan.FromSeconds(1.0f));
+		}
 	}
 }
